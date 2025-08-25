754d3e044381efca7a36c1aac837476d7a26ce20
Vic Chen <victor72441299@gmail.com>
2025-08-26 00:20:13 +0800

    Feat: clean code and fix some post content

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -11,8 +11,6 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${tagSlug}`}
-      // className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 mr-3 text-sm font-medium uppercase"
-      // className="border-primary-500 text-primary-600 hover:bg-primary-100 dark:border-primary-400 dark:text-primary-300 dark:hover:bg-primary-900/40 mr-2 mb-2 rounded-full border px-3 py-1 text-xs font-medium uppercase transition-colors"
       className="bg-primary-500 hover:bg-primary-600 dark:bg-primary-600 dark:hover:bg-primary-800 mr-2 mb-2 inline-block rounded-full px-3 py-1 text-xs font-medium text-white uppercase transition-colors"
     >
       {displayText}
