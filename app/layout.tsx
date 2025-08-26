47efd74d5a67709db37609b3b91ab5c8f3ae3276
Vic Chen <vicchen@cherricorp.com>
2025-08-26 11:11:05 +0800

    Feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -104,6 +105,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </SearchProvider>
             <Footer />
           </SectionContainer>
+          <SpeedInsights />
         </ThemeProviders>
       </body>
     </html>
